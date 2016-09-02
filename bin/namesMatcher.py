1b788a6f950da35d37043a64bc539b91c08c4fbd
Joost van Griethuysen <j.v.griethuysen@nki.nl>
2016-09-02 11:33:03 +0200

    ENH: Add support for additional baseline features
    Add "AddBaselineFeatures.csv"
    Refactor/Rename "matlab" to "baseline", this makes it more clear which parts are based solely on the matlab baseline, and what is indicative of the enitre baseline (i.e. matlab + additional baseline)

--- bin/namesMatcher.py
+++ bin/namesMatcher.py
@@ -1,16 +1,16 @@
 '''
 This is a helper function to extract classes/features names from both
 Matlab saved results and those supported by pyradiomics, and save them
-into indexed text files of name format <matlab|pyradiomics>_<feature_class>.txt.
+into indexed text files of name format <baseline|pyradiomics>_<feature_class>.txt.
 The extraction process aims to separate the prefixes related to the
 pre-processing done on the images from the feature calculation.
 
 The files have format <feature_number>:<feature_name>.
 
 The idea is next to create manually files of name format
-matlab2pyradiomics_<feature_class>.txt with the content structured as
+baseline2pyradiomics_<feature_class>.txt with the content structured as
 
-<matlab_feature_number>:<pyradiomics_feature_number>
+<baseline_feature_number>:<pyradiomics_feature_number>
 '''
 
 
@@ -19,6 +19,7 @@ import sys, os
 
 dataDir = os.path.join(os.path.dirname(os.path.abspath(__file__)),"..","data")
 matlabFeaturesFile = os.path.join(dataDir,"MatlabFeatures.csv")
+addBaselineFeaturesFile = os.path.join(dataDir, "AddBaselineFeatures.csv")
 outputDir = os.path.join(dataDir,'mapping')
 
 # mapping from the feature class names used in Matlab into pyradiomics class
@@ -27,17 +28,21 @@ outputDir = os.path.join(dataDir,'mapping')
 # pyradiomics feature class (at least this was the assumption of @fedorov)
 classMap = {"firstorder":"firstorder","Shape":"shape","RLGL":"rlgl","GLCM":"glcm","GLSZM":"glszm","rlgl":"rlgl","glcm":"glcm","glszm":"glszm","Stats":"firstorder","shape":"shape"}
 
-matlabFeaturesNamesList = open(matlabFeaturesFile,'r').readline()[:-1].split(',')
+# Read header from Matlab baseline features file
+# If present, update with header from Additional baseline features file
+baselineFeaturesNamesList = set(open(matlabFeaturesFile,'r').readline()[:-1].split(','))
+if os.path.exists(addBaselineFeaturesFile):
+  baselineFeaturesNamesList.update(set(open(addBaselineFeaturesFile, 'r').readline()[:-1].split(',')))
 
-# parse names from Matlab csv file
-matlabNames = {}
-matlab2pyradiomics_features = {}
-for n in matlabFeaturesNamesList:
+# parse names from baseline csv file(s)
+baselineNames = {}
+baseline2pyradiomics_features = {}
+for n in baselineFeaturesNamesList:
   # try to figure out the class name first
   classNameFound = False
-  for matlabClassName in classMap.keys():
-    if n.find(matlabClassName)>= 0:
-      n = n.split(matlabClassName+'_')[1]
+  for baselineClassName in classMap.keys():
+    if n.find(baselineClassName)>= 0:
+      n = n.split(baselineClassName+'_')[1]
       classNameFound = True
       break
 
@@ -47,21 +52,21 @@ for n in matlabFeaturesNamesList:
     continue
 
   # save this particular set of feature name mappings
-  matlab2pyradiomics_features[matlabClassName] = classMap[matlabClassName]
+  baseline2pyradiomics_features[baselineClassName] = classMap[baselineClassName]
 
-  category = matlabClassName
+  category = baselineClassName
   name = n
 
   try:
-    matlabNames[classMap[category]].add(name)
+    baselineNames[classMap[category]].add(name)
   except:
-    matlabNames[classMap[category]] = set()
-    matlabNames[classMap[category]].add(name)
+    baselineNames[classMap[category]] = set()
+    baselineNames[classMap[category]].add(name)
 
 # save the feature class mapping
-matlab2pyradiomics_featuresFile = os.path.join(outputDir, 'matlab2pyradiomics_featureClasses.txt')
-f = open(matlab2pyradiomics_featuresFile,'w')
-for m,p in matlab2pyradiomics_features.iteritems():
+baseline2pyradiomics_featuresFile = os.path.join(outputDir, 'baseline2pyradiomics_featureClasses.txt')
+f = open(baseline2pyradiomics_featuresFile,'w')
+for m,p in baseline2pyradiomics_features.iteritems():
   f.write(m+":"+p+"\n")
 f.close()
 
@@ -75,18 +80,18 @@ pyradiomicsNames['rlgl'] = rlgl.RadiomicsRLGL.getFeatureNames()
 
 # save feature names for individual classes, indexed
 #  with the keys to be used for mapping
-for k,v in matlabNames.iteritems():
+for k,v in baselineNames.iteritems():
   i = 0
-  featureListFile = os.path.join(outputDir,'matlab_'+k+'.txt')
+  featureListFile = os.path.join(outputDir,'baseline_'+k+'.txt')
   f = open(featureListFile,'w')
-  mappingFile = os.path.join(outputDir,'matlab2pyradiomics_'+k+'.txt')
+  mappingFile = os.path.join(outputDir,'baseline2pyradiomics_'+k+'.txt')
   m = open(mappingFile,'w')
   for fn in v:
     f.write(str(i)+":"+fn+"\n")
     i = i+1
     # if there is an exact match, save the mapping
     if fn in pyradiomicsNames[k]:
-      m.write(str(list(matlabNames[k]).index(fn)))
+      m.write(str(list(baselineNames[k]).index(fn)))
       m.write(':')
       m.write(str(list(pyradiomicsNames[k]).index(fn)))
       m.write('\n')

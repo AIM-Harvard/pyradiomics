888eea61677b5b3d52b03eef5446b12e2a8d6775
Joost van Griethuysen <j.v.griethuysen@nki.nl>
2016-10-24 14:52:51 +0200

    ENH: Update test_features, enabling all test cases

--- tests/test_features.py
+++ tests/test_features.py
@@ -15,9 +15,7 @@ defaultTestCases = testUtils.getTestCases()
 defaultFeatures = ["firstorder", "glcm", "glrlm", "shape", "glszm"]
 
 testCases = defaultTestCases
-# testCases = ["breast1"]
-#features = defaultFeatures
-features = ["shape"]
+features = defaultFeatures
 
 featureClass = None
 

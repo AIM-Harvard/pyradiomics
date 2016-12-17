d611f07aa47171d7d09260546275e3b9503b9c53
Joost van Griethuysen <j.v.griethuysen@nki.nl>
2016-12-18 00:02:51 +0100

    ENH: Make testing of features dynamic
    Use dynamic finding and loading of feature classes in `featureextractor.py` to dynamically test the features. Only dynamic finding functionality of `featureextractor` is used. Preprocessing is kept in `testUtils` and feature classes are instantiated by `test_features`

--- tests/test_features.py
+++ tests/test_features.py
@@ -2,7 +2,7 @@
 # setenv PYTHONPATH /path/to/pyradiomics/radiomics
 # nosetests --nocapture -v tests/test_features.py
 
-from radiomics import firstorder, glcm, glrlm, shape, glszm, gldm, ngtdm, gldzm
+from radiomics.featureextractor import RadiomicsFeaturesExtractor
 from testUtils import RadiomicsTestUtils
 import os
 import logging
@@ -10,59 +10,43 @@ from nose_parameterized import parameterized
 import SimpleITK as sitk
 
 testUtils = RadiomicsTestUtils()
-defaultTestCases = testUtils.getTestCases()
-defaultFeatures = ["firstorder", "glcm", "glrlm", "shape", "glszm", "gldm", "ngtdm", "gldzm"]
+testUtils.setResampling(resampledPixelSpacing=None,  # resampledPixelSpacing= [3, 3, 3]
+                        interpolator=sitk.sitkBSpline)
+testCases = testUtils.getTestCases()
 
-testCases = defaultTestCases
-features = defaultFeatures
-
-featureClass = None
-
-# set testing arguments
 kwargs = {}
 kwargs['binWidth'] = 25
 kwargs['symmetricalGLCM'] = False  # Current baseline is based upon assymetrical GLCM
 kwargs['verbose'] = False
 
-testUtils.setResampling(resampledPixelSpacing= None,interpolator= sitk.sitkBSpline)  # resampledPixelSpacing= [3, 3, 3]
+extractor = RadiomicsFeaturesExtractor(**kwargs)
+
+featureClass = None
+
 
 class TestFeatures:
-
   def generate_scenarios():
     global testCases
-    global features
 
     for testCase in testCases:
-      for feature in features:
-        featureNames = None
-        if feature == 'firstorder':
-          featureNames = firstorder.RadiomicsFirstOrder.getFeatureNames()
-        elif feature == 'glcm':
-          featureNames = glcm.RadiomicsGLCM.getFeatureNames()
-        elif feature == 'glrlm':
-          featureNames = glrlm.RadiomicsGLRLM.getFeatureNames()
-        elif feature == 'shape':
-          featureNames = shape.RadiomicsShape.getFeatureNames()
-        elif feature == 'glszm':
-          featureNames = glszm.RadiomicsGLSZM.getFeatureNames()
-        elif feature == 'gldm':
-          featureNames = gldm.RadiomicsGLDM.getFeatureNames()
-        elif feature == 'ngtdm':
-          featureNames = ngtdm.RadiomicsNGTDM.getFeatureNames()
-        elif feature == 'gldzm':
-          featureNames = gldzm.RadiomicsGLDZM.getFeatureNames()
-        assert(featureNames != None)
+      for featureClassName, featureClass in extractor.featureClasses.iteritems():
+        featureNames = featureClass.getFeatureNames()
+        assert (featureNames != None)
+        assert (len(featureNames) > 0)
         logging.debug('generate_scenarios: featureNames = %s', featureNames)
         for featureName in featureNames:
-          yield testCase, feature, featureName
+          yield testCase, featureClassName, featureName
 
   global testUtils
+
   @parameterized.expand(generate_scenarios(), testcase_func_name=testUtils.custom_name_func)
   def test_scenario(self, testCase, featureClassName, featureName):
     print("")
     global testUtils
+    global extractor
 
-    logging.debug('test_scenario: testCase = %s, featureClassName = %s, featureName = %s', testCase, featureClassName, featureName)
+    logging.debug('test_scenario: testCase = %s, featureClassName = %s, featureName = %s', testCase, featureClassName,
+                  featureName)
 
     testCaseChanged = testUtils.setTestCase(testCase)
     featureChanged = testUtils.setFeatureClassName(featureClassName)
@@ -71,30 +55,8 @@ class TestFeatures:
     testImage = testUtils.getImage()
     testMask = testUtils.getMask()
     if featureClass is None or testCaseChanged or featureChanged:
-      if featureClassName == 'firstorder':
-        logging.debug('Init First Order')
-        featureClass = firstorder.RadiomicsFirstOrder(testImage, testMask, **kwargs)
-      elif featureClassName == 'glcm':
-        logging.debug('Init GLCM')
-        featureClass = glcm.RadiomicsGLCM(testImage, testMask, **kwargs)
-      elif featureClassName == 'glrlm':
-        logging.debug('Init GLRLM')
-        featureClass = glrlm.RadiomicsGLRLM(testImage, testMask, **kwargs)
-      elif featureClassName == 'shape':
-        logging.debug('Init Shape')
-        featureClass = shape.RadiomicsShape(testImage, testMask, **kwargs)
-      elif featureClassName == 'glszm':
-        logging.debug('Init GLSZM')
-        featureClass = glszm.RadiomicsGLSZM(testImage, testMask, **kwargs)
-      elif featureClassName == 'gldm':
-        logging.debug('Init GLDM')
-        featureClass = gldm.RadiomicsGLDM(testImage, testMask, **kwargs)
-      elif featureClassName == 'ngtdm':
-        logging.debug('Init NGTDM')
-        featureClass = ngtdm.RadiomicsNGTDM(testImage, testMask, **kwargs)
-      elif featureClassName == 'gldzm':
-        logging.debug('Init GLDZM')
-        featureClass = gldzm.RadiomicsGLDZM(testImage, testMask, **kwargs)
+      logging.debug('Init %s' % (featureClassName))
+      featureClass = extractor.featureClasses[featureClassName](testImage, testMask, **kwargs)
 
     assert (featureClass != None)
 
@@ -105,15 +67,26 @@ class TestFeatures:
     val = featureClass.featureValues[featureName]
     testUtils.checkResult(featureName, val)
 
+
 def teardown_module():
+  global testUtils
+  global extractor
+  global featureClass
   print("")
   res = testUtils.getResults()
+  print 'Results:'
   print res
   resultsFile = os.path.join(testUtils.dataDir, 'PyradiomicsFeatures.csv')
   testUtils.writeCSV(res, resultsFile)
   diff = testUtils.getDiffs()
   print 'Differences from baseline:'
   print diff
-  diffFile = os.path.join(testUtils.dataDir, 'Matlab2PyradiomicsFeaturesDiff.csv')
+  diffFile = os.path.join(testUtils.dataDir, 'Baseline2PyradiomicsFeaturesDiff.csv')
   testUtils.writeCSV(diff, diffFile)
-  logging.info("Wrote calculated features to %s, and the differences between the matlab features and the pyradiomics ones to %s.", resultsFile, diffFile)
+  logging.info(
+    "Wrote calculated features to %s, and the differences between the matlab features and the pyradiomics ones to %s.",
+    resultsFile, diffFile)
+  # Clean up
+  testUtils = None
+  extractor = None
+  featureClass = None

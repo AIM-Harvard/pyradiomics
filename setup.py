b0c7fdca1338914b30eef84b621382a1b5b1cd9e
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-02-13 20:18:04 -0500

    STYL: setup.py: Fix style to be PEP8 compliant and improve readability

--- setup.py
+++ setup.py
@@ -1,22 +1,37 @@
+#!/usr/bin/env python
+
 import versioneer
 
 from setuptools import setup
 
-with open('requirements.txt', 'r') as fp:
-  requirements = list(filter(bool, (line.strip() for line in fp)))
 
-setup(name='pyradiomics',
-      version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
-      packages=['radiomics', 'radiomics.scripts'],
-      entry_points={'console_scripts': ['pyradiomics=radiomics.scripts.commandline:main',
-                                        'pyradiomicsbatch=radiomics.scripts.commandlinebatch:main']},
-      data_files=[
-        ('data', ['data/paramSchema.yaml', 'data/schemaFuncs.py'])],
-      description='Radiomics features library for python',
-      url='http://github.com/Radiomics/pyradiomics',
-      author='pyradiomics community',
-      author_email='pyradiomics@googlegroups.com',
-      license='Slicer',
-      zip_safe=False,
-      install_requires=requirements)
+with open('requirements.txt', 'r') as fp:
+    requirements = list(filter(bool, (line.strip() for line in fp)))
+
+setup(
+    name='pyradiomics',
+
+    url='http://github.com/Radiomics/pyradiomics#readme',
+
+    author='pyradiomics community',
+    author_email='pyradiomics@googlegroups.com',
+
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+
+    packages=['radiomics', 'radiomics.scripts'],
+    zip_safe=False,
+    data_files=[
+      ('data', ['data/paramSchema.yaml', 'data/schemaFuncs.py'])],
+
+    entry_points={
+        'console_scripts': [
+            'pyradiomics=radiomics.scripts.commandline:main',
+            'pyradiomicsbatch=radiomics.scripts.commandlinebatch:main'
+        ]},
+
+    description='Radiomics features library for python',
+    license='Slicer',
+
+    install_requires=requirements
+)

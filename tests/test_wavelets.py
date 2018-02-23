01aa7757d5b9c56c3257a482041b1f2ed87ebaee
Michael Schwier <mschwier@bwh.harvard.edu>
2018-02-23 12:02:55 -0500

    STYLE: Fixing complaints of flake8

--- tests/test_wavelets.py
+++ tests/test_wavelets.py
@@ -1,16 +1,18 @@
-import radiomics
-import pandas as pd
 import itertools
+
+import pandas as pd
+
+import radiomics
+
 
 class TestWaveletsOnROIs:
 
   def setup(self):
     self.testData = []
-    self.testData.append({"image": "../data/test-image_64x64x64.nrrd", 
+    self.testData.append({"image": "../data/test-image_64x64x64.nrrd",
                           "mask": "../data/test-mask_64x64x64.nrrd"})
-    self.testData.append({"image": "../data/test-image_37x37x37.nrrd", 
+    self.testData.append({"image": "../data/test-image_37x37x37.nrrd",
                           "mask": "../data/test-mask_37x37x37.nrrd"})
-
 
   def test_features_equal_on_various_rois_of_same_image(self):
     extractors = []
@@ -24,8 +26,8 @@ class TestWaveletsOnROIs:
     features = []
     for extractor in extractors:
       for data in self.testData:
-        features.append(extractor.execute(data["image"], data["mask"], label = 1))
-    
+        features.append(extractor.execute(data["image"], data["mask"], label=1))
+
     dfFeatures = pd.DataFrame(features)
     for column in dfFeatures:
       if not column.startswith("general"):
@@ -33,16 +35,11 @@ class TestWaveletsOnROIs:
         maxDiff = max(abs(a) - abs(b) for a, b in itertools.combinations(featList, 2))
         assert maxDiff == 0
 
-
   def getTestExtrator(self, settings):
     extractor = radiomics.featureextractor.RadiomicsFeaturesExtractor(**settings)
     extractor.disableAllImageTypes()
     extractor.disableAllFeatures()
     extractor.enableImageTypes(Wavelet={})
-    extractor.enableFeaturesByName(firstorder=['Median','Mean','Minimum','Maximum'])
-    extractor.enableFeaturesByName(glcm=['Contrast','Correlation','JointEnergy','JointEntropy','Idm'])
+    extractor.enableFeaturesByName(firstorder=['Median', 'Mean', 'Minimum', 'Maximum'])
+    extractor.enableFeaturesByName(glcm=['Contrast', 'Correlation', 'JointEnergy', 'JointEntropy', 'Idm'])
     return extractor
-
-  
-
-

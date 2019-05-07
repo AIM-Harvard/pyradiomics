5c6b09274310d8908578eda19d056c9fc7bd6b09
Joost van Griethuysen <joostjm@gmail.com>
2019-05-07 11:15:27 +0200

    CircleCI deploy conda test

--- conda/configure_conda.bat
+++ conda/configure_conda.bat
@@ -2,6 +2,7 @@ conda config --set always_yes yes --set changeps1 no
 conda config --set anaconda_upload no
 conda config --add channels simpleitk
 conda config --add channels conda-forge
+conda install gcc_linux-64 gxx_linux64 libgcc
 conda install conda-build
 conda install anaconda-client
 conda update -q conda

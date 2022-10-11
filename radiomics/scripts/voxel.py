9e9b3eb137e9f1a040317d9db5aee3c94c6bad4e
Joost van Griethuysen <joostjm@gmail.com>
2022-10-11 18:36:26 +0200

    BUG: Fix label_channel selection bug
    When Label_channel column is present in the CSV input, it needs to be parsed as an int.
    However, due to a bug, the value for label was used instead. Related to 54a37822, but now for voxel-based extraction

--- radiomics/scripts/voxel.py
+++ radiomics/scripts/voxel.py
@@ -39,7 +39,7 @@ def extractVoxel(case_idx, case, extractor, **kwargs):
       label = int(label)
     label_channel = case.get('Label_channel', None)  # Optional
     if isinstance(label_channel, six.string_types):
-      label_channel = int(label)
+      label_channel = int(label_channel)
 
     # Extract features
     result = extractor.execute(imageFilepath, maskFilepath, label, label_channel, voxelBased=True)

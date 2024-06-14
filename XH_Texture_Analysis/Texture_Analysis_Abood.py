ca20c73ce593b026e52f4cc5b5084f3f7c7a279b
XavierH20 <franciscoxavier.hoyoscordon@cchmc.org>
2024-06-14 16:50:31 -0400

    Changes to detect defects. Need to work on geometryTolerance

--- XH_Texture_Analysis/Texture_Analysis_Abood.py
+++ XH_Texture_Analysis/Texture_Analysis_Abood.py
@@ -50,7 +50,7 @@ for numImage in range(maskVent_control.shape[0]):
 defect_path = os.path.join(path_control, 'VDPThresholdAnalysis.mat')
 threshold_mat = loadmat(defect_path)
 defect = np.array(threshold_mat['defectArray'])
-
+defect[defect != 0] = 1
 mask_defect_control = np.zeros_like(maskVent_control)
 
 # Sanity check to observe the original mask without the ventilation defects
@@ -135,7 +135,7 @@ for numImage in range(maskVent_Lam.shape[0]):
 defect_path = os.path.join(path_Lam, 'VDPThresholdAnalysis.mat')
 threshold_mat = loadmat(defect_path)
 defect = np.array(threshold_mat['defectArray'])
-
+defect[defect != 0] = 1
 mask_defect_Lam = np.zeros_like(maskVent_Lam)
 
 # Sanity check to observe the original mask without the ventilation defects
@@ -177,15 +177,98 @@ df = pd.DataFrame(featureVector.keys(),columns=['Features'])
 df_2 = pd.DataFrame(featureVector.values(),columns=['Measurements'])
 df_merged_Lam = pd.concat([df, df_2], axis=1)
 
+
+# %% Import ventilation image and mask
+# Very patchy Lam patient 
+print('Lam patient patchy')
+path = '/home/hoyeh3/GitHub-wsl2/pyradiomics/XH_Texture_Analysis'
+path_Lam_patchy = os.path.join(path, 'Lam_test_patchy')
+
+imagefile_Lam_patchy = 'VentilationImage.nii.gz'
+maskfile_Lam_patchy = 'MaskImage.nii.gz'
+
+imageName_Lam_patchy = sitk.ReadImage(os.path.join(path_Lam_patchy, imagefile_Lam_patchy))
+maskName_Lam_patchy = sitk.ReadImage(os.path.join(path_Lam_patchy, maskfile_Lam_patchy))
+
+# Sanity check
+imageVent_Lam_patchy = sitk.GetArrayFromImage(imageName_Lam_patchy)
+maskVent_Lam_patchy = sitk.GetArrayFromImage(maskName_Lam_patchy)
+for numImage in range(maskVent_Lam_patchy.shape[0]):
+  plt.figure(figsize=(10,10))
+  plt.subplot(1,3,1)
+  plt.imshow(imageVent_Lam_patchy[numImage,:,:], cmap="gray")
+  plt.title(f"Ventilation #{numImage}")
+  plt.subplot(1,3,2)
+  plt.imshow(maskVent_Lam_patchy[numImage,:,:])        
+  plt.title(f"Mask #{numImage}")
+  plt.subplot(1,3,3)
+  plt.imshow(imageVent_Lam_patchy[numImage,:,:]*maskVent_Lam_patchy[numImage,:,:])        
+  plt.title(f"Mask and ventilation #{numImage}")
+
+# %% Import the ventilation defects from Matlab
+defect_path = os.path.join(path_Lam_patchy, 'VDPThresholdAnalysis.mat')
+threshold_mat = loadmat(defect_path)
+defect = np.array(threshold_mat['defectArray'])
+defect[defect != 0] = 1
+mask_defect_Lam_patchy = np.zeros_like(maskVent_Lam_patchy)
+
+# Sanity check to observe the original mask without the ventilation defects
+for numImage in range(maskVent_Lam_patchy.shape[0]):
+  plt.figure(figsize=(10,10))
+  plt.subplot(1,3,1)
+  plt.title(f"Defect #{numImage}")
+  plt.imshow(defect[:,:,numImage],cmap='gray')
+  plt.subplot(1,3,2)
+  plt.imshow(maskVent_Lam_patchy[numImage,:,:],cmap='gray')        
+  plt.title(f"Mask #{numImage}")
+  plt.subplot(1,3,3)
+  mask_defect_Lam_patchy[numImage,:,:] = abs(maskVent_Lam_patchy[numImage,:,:]-defect[:,:,numImage])
+  plt.imshow(imageVent_Lam_patchy[numImage,:,:]*mask_defect_Lam_patchy[numImage,:,:])        
+  plt.title(f"Mask-defect #{numImage}")
+
+# Transform the array from the mask minus the vent. defect into an sitk Image
+mask_defect_sitk_Lam_patchy = sitk.GetImageFromArray(mask_defect_Lam_patchy)
+
+# %% Obtain radiomics properties
+# Get the location of the settings file
+paramsFile = os.path.abspath(os.path.join(path,'Settings', 'Params.yaml'))
+if imageName_Lam_patchy is None or maskName_Lam_patchy is None:
+  print('Error getting testcase!')
+  exit()
+
+# Initialize feature extractor using the settings file 'Params.yaml'
+extractor = featureextractor.RadiomicsFeatureExtractor(paramsFile)
+featureClasses = getFeatureClasses()
+
+# Print each calculation from the features
+print("Calculating features")
+featureVector = extractor.execute(imageName_Lam_patchy, mask_defect_sitk_Lam_patchy)
+
+for featureName in featureVector.keys():
+  print("Computed feature: %s " % (featureName))
+
+df = pd.DataFrame(featureVector.keys(),columns=['Features'])
+df_2 = pd.DataFrame(featureVector.values(),columns=['Measurements'])
+df_merged_Lam_patchy = pd.concat([df, df_2], axis=1)
+
+
+
+
+
+
 # %% Store features into an Excel document
 
 #Remove packages versions and original image and mask data, NOT radiomics
 df_merged_control_filt = df_merged_control.iloc[22:]
 df_merged_Lam_filt = df_merged_Lam.iloc[22:]
+df_merged_Lam_filt_patchy = df_merged_Lam_patchy.iloc[22:]
 
 # Save data in excel
 with pd.ExcelWriter(path+'/Features.xlsx') as writer:
     df_merged_control.to_excel(writer, sheet_name="Control", index=False)
     df_merged_Lam.to_excel(writer, sheet_name="Lam", index=False)
+    df_merged_Lam_patchy.to_excel(writer, sheet_name="Lam", index=False)
     df_merged_control_filt.to_excel(writer, sheet_name="Control_filt", index=False)
     df_merged_Lam_filt.to_excel(writer, sheet_name="Lam_filt", index=False)
+    df_merged_Lam_filt_patchy.to_excel(writer, sheet_name="Lam_filt", index=False)
+

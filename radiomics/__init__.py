1741696fa8e5a46b2da62ac2b87243b735c8749e
Kozmosa <xuyang1638@gmail.com>
2025-02-25 22:53:33 +0800

    update versionner to latest version(==0.29) to fix configparser issue: SafeConfigParser -> RawConfigParser

--- radiomics/__init__.py
+++ radiomics/__init__.py
@@ -307,3 +307,6 @@ from ._version import get_versions  # noqa: I202
 
 __version__ = get_versions()['version']
 del get_versions
+
+from . import _version
+__version__ = _version.get_versions()['version']
